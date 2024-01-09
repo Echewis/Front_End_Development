431c05a81dd18b6655f34fb6fb6091df475f0c81
Wiz <echewis24@gmail.com>
2024-01-09 22:07:55 +0100

    update

--- single_page_website/script.js
+++ single_page_website/script.js
@@ -6,14 +6,13 @@ function addRecommendation() {
       console.log("New recommendation added");
       showPopup(true);
   
-      // Create a new 'recommendation' element and set it's value to the user's message
+      // This will create a new 'recommendation' element and set it's value to the user's message
       var element = document.createElement("div");
       element.setAttribute("class","recommendation");
       element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-      // Add this element to the end of the list of recommendations
+
       document.getElementById("all_recommendations").appendChild(element); 
       
-      // Reset the value of the textarea
       recommendation.value = "";
     }
   }
